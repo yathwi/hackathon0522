c64e10ad898a401765e001baf3d2a43b5012c9ce
Ryosuke Yoshizaki <ryosuke.yoshizaki@kikagaku.co.jp>
2022-02-10 11:26:35 +0900

    トップページの更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
-const Home: React.FC = () => <h1>Welcome to Kikagaku Next.js Starter Kit!!</h1>;
+const Home: React.FC = () => (
+  <main className='mx-auto max-w-6xl py-6'>
+    <h1 className='text-lg font-bold text-primary-800'>
+      Welcome to Kikagaku Next.js Starter Kit!!
+    </h1>
+  </main>
+);
 
 export default Home;
