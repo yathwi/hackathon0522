74a71c20f627a096cf283ffb4082a67b1fb413db
Ryosuke Yoshizaki <ryosuke.yoshizaki@kikagaku.co.jp>
2022-02-16 15:54:10 +0900

    Storybook の設定を最新版に更新

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,13 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-  ],
-  framework: '@storybook/react',
+  stories: ['../src/component/**/*.stories.mdx', '../src/component/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-next'],
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin({ extensions: config.resolve.extensions })];
+    return config;
+  },
 };
